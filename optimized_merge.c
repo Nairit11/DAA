20e5bd17770aee062b1cc4539c94ca03af19543a
Nairit Banerjee <nairit32@gmail.com>
2018-01-28 15:37:34 +0000

    An optimized code for merging two sorted sets

---
+++ optimized_merge.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct node{
+	int data;
+	struct node *next;
+	struct node *prev;
+};
+
+int main(){
+	int n,m,i;
+	struct node *head1, *tail1, *head2, *tail2;
+	printf("Enter size and elements of set1\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+
+	}
+	printf("Enter size and elements of set2\n");
+	scanf("%d",&m);
+	for(i=0;i<m;i++){
+		
+	}
+	return 0;
+}
