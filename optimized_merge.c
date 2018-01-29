e42560bc9457e64ac08f26c7c08344fd16076483
Nairit Banerjee <nairit32@gmail.com>
2018-01-29 11:52:09 +0000

    Update optimized_merge.c

--- optimized_merge.c
+++ optimized_merge.c
@@ -28,20 +28,85 @@ struct node* insert(struct node *head, int data,struct node *tail){
 	}
 }
 
+void display(struct node *head){
+	struct node *p=head;
+	while(p!=NULL){
+		printf("%d ",p->data);
+		p=p->next;
+	}
+	printf("\n");
+
+}
+
+struct node* merge(struct node *head1,struct node *head2){
+	struct node *p=head1;
+	struct node *q=head2;
+	struct node *cur=head1;
+	if(head2->data >= tail1->data){
+		tail1->next=head2;
+		head2->prev=tail1;
+		return head1;
+	}
+	if(q->data < p->data){
+		printf("Hello");
+		p->prev=q;
+		q->next=p;
+		p=q;
+		q=q->next;
+		p->prev=NULL;
+		head1=p;
+		
+	}
+
+	while(q!=NULL){
+		if(q->data >= p->data){
+			if(p->next!=NULL)
+				p=p->next;
+			else
+				break;
+		}
+		else{
+			struct node *node=(struct node*) malloc(sizeof(struct node));
+			node->data=q->data;
+			p->prev->next=node;
+			node->prev=p->prev;
+			p->prev=node;
+			node->next=p;
+			q=q->next;
+		}
+	}
+	if(q!=NULL){
+
+		p->next=q;
+		q->prev=p;
+	}
+	return head1;
+}
+
 int main(){
-	int n,m,i;
+	int n,m,i,a;
 	//struct node *head1, *tail1, *head2, *tail2;
 	printf("Enter size and elements of set1\n");
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
-		scanf(%d,&a);
+		scanf("%d",&a);
 		head1=insert(head1,a,tail1);
 	}
 	printf("Enter size and elements of set2\n");
 	scanf("%d",&m);
 	for(i=0;i<m;i++){
-		scanf(%d,&a);
-		head2=insert(head2,a);
+		scanf("%d",&a);
+		head2=insert(head2,a,tail2);
 	}
+	if(n>m){
+		head1=merge(head1,head2);
+		display(head1);
+	}
+	else{
+		head2=merge(head2,head1);
+		display(head2);
+	}
+	//display(head1);
+	//display(head2);
 	return 0;
 }
