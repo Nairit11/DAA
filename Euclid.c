5a65083324c4645205f7f5e7c856790ccdd27f27
Nairit Banerjee <nairit32@gmail.com>
2018-03-01 18:37:25 +0530

    Euclid's algo

--- Euclid.c
+++ Euclid.c
@@ -18,28 +18,19 @@ int GCD(int A, int B) {
 int main()
 {
 	int n,m,ctr=15;
-	FILE *f2,*f3,*f4,*f5;
+	FILE *f2,*f1;
 	f2=fopen("input.txt","r");
-    f3=fopen("best.txt","w");
-    f4=fopen("average.txt","w");
-    f5=fopen("worst.txt","w");
+    f1=fopen("output.txt","w");
     while(ctr>0){
 	    fscanf(f2,"%d%d",&n,&m);
 	    t=t++;	
 	    printf("GCD of %d and %d is %d\n",n,m,GCD(m,n));
 	    ctr--;
-	    if(ctr>10)
-	    	fprintf(f3,"%d	%d\n",number_of_recursivecall,t);
-	    else if(ctr>5 && ctr<11)
-	    	fprintf(f4,"%d	%d\n",number_of_recursivecall,t);
-	    else
-	    	fprintf(f5,"%d	%d\n",number_of_recursivecall,t);
+		fprintf(f1,"%d	%d\n",number_of_recursivecall,t);
 	    t=0;
 	    number_of_recursivecall=0;
 	}
     fclose(f2);
-    fclose(f3);
-    fclose(f4);
-    fclose(f5);
+    fclose(f1);
     return 0;
 }
