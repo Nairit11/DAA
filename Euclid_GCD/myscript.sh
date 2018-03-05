c0d641742b87f29b7c87058977310988ab65959c
Nairit11 <nairit32@gmail.com>
2018-03-05 13:11:36 +0530

    Algorithm to find GCD using Euclid's method

--- Euclid_GCD/myscript.sh
+++ Euclid_GCD/myscript.sh
@@ -3,6 +3,6 @@ gnuplot -e "set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 0.5;
 set style line 2 lc rgb '#dd181f' lt 1 lw 2 pt 5 ps 0.5;
 set style line 4 lt 2 lc rgb '#FF00FF' lw 2 ps 0.5;
 set xlabel 'n';
-set ylabel 'running time (secs)';
-plot '~/Desktop/DAA/Euclid_GCD/best.txt' title 'Best' with linespoints ls 1, '~/Desktop/DAA/Euclid_GCD/average.txt' title 'Average' with linespoints ls 2, '~/Desktop/DAA/Euclid_GCD/worst.txt' title 'Worst' with linespoints ls 4;
+set ylabel 'running time (units)';
+plot '~/Desktop/DAA/Euclid_GCD/output.txt' title 'Running time' with linespoints ls 1;
 pause -1"
